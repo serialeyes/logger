ffd0564ce32e23a7f9ed5c83908fef6df03b060a
serialeyes <serialeyes@proton.me>
2023-04-30 20:06:46 -0400

    adding LogToFile()

--- logger.go
+++ logger.go
@@ -9,3 +9,13 @@ var Version string = "1.0"
 func Log(mess string) {
     fmt.Println("[LOG] " + mess)
 }
+
+func LogToFile(mess string, dir string) {
+    f, err := os.Create(dir + "/log.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer f.Close()
+
+    f.WriteString(mess + "\n")
+}
